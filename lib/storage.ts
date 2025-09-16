a149259a62c93bfdb40cdaaef9873ac5ff3fd4cd
Brenda <brendacpereira@hotmail.com>
2025-09-16 09:38:29 -0300

    atualização de pastas

---
+++ lib/storage.ts
@@ -0,0 +1,16 @@
+import {book} from "@/app/types/books";
+
+export const books: book[] = [
+    {
+    id: "1",
+    title: "Capitães da Areia",
+    author: "Jorge Amado",
+    genre: "Literatura Brasileira",
+    year: 1937,
+    pages: 283,
+    rating: 5,
+    synopsis: "Um clássico da literatura brasileira .",
+    cover: "/covers/capitaes-da-areia.jpg",
+    status: "LIDO",
+    }
+];
