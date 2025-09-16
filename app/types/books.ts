a9014edf6ac9b6e03176d07117305dfd318062e7
Thallysson <thallyssonluizfernandesth@gmail.com>
2025-09-15 21:20:55 -0300

    Primeiro commit do projeto BookShelf

---
+++ app/types/books.ts
@@ -0,0 +1,12 @@
+export type book = {
+    id: string,
+    title: string,
+    author: string,
+    genre: string,
+    year: number,
+    pages: number,
+    rating: number,
+    synopsis: string,
+    cover: string,
+    status: Selection,
+}
